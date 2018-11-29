1459c597c9aecb031d32e3adc4fb3e959a09de64
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-11-29 17:08:43 +1100

    Refactor off-policy prediction

--- Unit-6-The-Windy-Gridworld/off_policy_prediction.py
+++ Unit-6-The-Windy-Gridworld/off_policy_prediction.py
@@ -1,34 +1,37 @@
-from windygrid import WindyGrid
-from utils import printPolicy, printQ, sampleReducedActionSpace
 import numpy as np
+from windy_grid import WindyGrid
+from utils import print_estimates
 
-if __name__ == '__main__':
-    grid = WindyGrid(6,6, wind=[0,0,1,2,1,0])
-    GAMMA = 0.9
+if __name__ == "__main__":
+    GRID = WindyGrid(6, 6, wind=[0, 0, 1, 2, 1, 0])
+    DISCOUNT = 0.9
 
-    Q = {}
-    C = {}
-    for state in grid.stateSpacePlus:
-        for action in grid.possibleActions:
-            Q[(state,action)] = 0
-            C[(state,action)] = 0
+    ESTIMATES = {}
+    C_ESTIMATES = {}
+    for state in GRID.total_state_space:
+        for action in GRID.possible_actions:
+            ESTIMATES[(state, action)] = 0
+            C_ESTIMATES[(state, action)] = 0
 
-    targetPolicy = {}
-    for state in grid.stateSpace:
-        targetPolicy[state] = np.random.choice(grid.possibleActions)
+    TARGET_POLICY = {}
+    for state in GRID.state_space:
+        TARGET_POLICY[state] = np.random.choice(GRID.possible_actions)
 
-    for i in range(1000):
-        if i % 100 == 0:
+    NUM_EPISODES = 1000
+    EPISODE_INTERVAL = 100
+
+    for i in range(NUM_EPISODES):
+        if i % EPISODE_INTERVAL == 0:
             print(i)
-        behaviorPolicy = {}
-        for state in grid.stateSpace:
-            behaviorPolicy[state] = grid.possibleActions
+        behavior_policy = {}
+        for state in GRID.state_space:
+            behavior_policy[state] = GRID.possible_actions
         memory = []
-        observation, done = grid.reset()
+        observation, done = GRID.reset()
         steps = 0
         while not done:
-            action = np.random.choice(behaviorPolicy[observation])
-            observation_, reward, done, info = grid.step(action)
+            action = np.random.choice(behavior_policy[observation])
+            observation_, reward, done, info = GRID.step(action)
             steps += 1
             if steps > 25:
                 done = True
@@ -37,18 +40,21 @@ if __name__ == '__main__':
             observation = observation_
         memory.append((observation, action, reward))
 
-        G = 0
-        W = 1
+        returns = 0
+        relative_probability = 1
         last = True
         for (state, action, reward) in reversed(memory):
             if last:
                 last = False
             else:
-                C[state,action] += W
-                Q[state,action] += (W / C[state,action])*(G-Q[state,action])
-                prob = 1 if action in targetPolicy[state] else 0
-                W *= prob/(1/len(behaviorPolicy[state]))
-                if W == 0:
+                C_ESTIMATES[state, action] += relative_probability
+                ESTIMATES[state, action] += (
+                    (relative_probability / C_ESTIMATES[state, action])
+                    * (returns - ESTIMATES[state, action])
+                )
+                prob = 1 if action in TARGET_POLICY[state] else 0
+                relative_probability *= prob / (1 / len(behavior_policy[state]))
+                if relative_probability == 0:
                     break
-            G = GAMMA*G + reward
-    printQ(Q, grid)
+            returns = DISCOUNT * returns + reward
+    print_estimates(ESTIMATES, GRID)
