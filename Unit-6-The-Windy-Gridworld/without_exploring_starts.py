fc612db8f4e26334b06d61c5846972a12ddfa76f
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-11-29 16:24:59 +1100

    Refactor without exploring starts

--- Unit-6-The-Windy-Gridworld/without_exploring_starts.py
+++ Unit-6-The-Windy-Gridworld/without_exploring_starts.py
@@ -1,38 +1,41 @@
-from windygrid import WindyGrid
-from utils import printPolicy, printQ, sampleReducedActionSpace
 import numpy as np
+from windy_grid import WindyGrid
+from utils import print_policy, print_estimates
 
 if __name__ == "__main__":
-    grid = WindyGrid(6,6, wind=[0, 0, 1, 2, 1, 0])
-    GAMMA = 0.9
-    EPS = 0.4
+    GRID = WindyGrid(6, 6, wind=[0, 0, 1, 2, 1, 0])
+    DISCOUNT = 0.9
+    EPSILON = 0.4
 
-    Q = {}
-    returns = {}
-    pairsVisited = {}
-    for state in grid.stateSpacePlus:
-        for action in grid.actionSpace.keys():
-            Q[(state, action)] = 0
-            returns[(state,action)] = 0
-            pairsVisited[(state,action)] = 0
+    ESTIMATES = {}
+    RETURNS = {}
+    PAIRS_VISITED = {}
+    for state in GRID.total_state_space:
+        for action in GRID.action_space:
+            ESTIMATES[(state, action)] = 0
+            RETURNS[(state, action)] = 0
+            PAIRS_VISITED[(state, action)] = 0
 
-    policy = {}
-    for state in grid.stateSpace:
-        policy[state] = grid.possibleActions
+    POLICY = {}
+    for state in GRID.state_space:
+        POLICY[state] = GRID.possible_actions
 
-    for i in range(1000000):
-        statesActionsReturns = []
-        if i % 100000 == 0:
+    NUM_EPISODES = 1000000
+    EPISODE_INTERVAL = 100000
+
+    for i in range(NUM_EPISODES):
+        states_actions_returns = []
+        if i % EPISODE_INTERVAL == 0:
             print("starting episode", i)
-        observation, done = grid.reset()
+        observation, done = GRID.reset()
         memory = []
         steps = 0
         while not done:
-            if len(policy[observation]) > 1:
-                action = np.random.choice(policy[observation])
+            if len(POLICY[observation]) > 1:
+                action = np.random.choice(POLICY[observation])
             else:
-                action = policy[observation]
-            observation_, reward, done, info = grid.step(action)
+                action = POLICY[observation]
+            observation_, reward, done, info = GRID.step(action)
             steps += 1
             if steps > 25 and not done:
                 done = True
@@ -43,30 +46,35 @@ if __name__ == "__main__":
         #append the terminal state
         memory.append((observation, action, reward))
 
-        G = 0
+        returns = 0
         last = True # start at t = T - 1
         for state, action, reward in reversed(memory):
             if last:
                 last = False
             else:
-                statesActionsReturns.append((state,action,G))
-            G = GAMMA*G + reward
-        statesActionsReturns.reverse()
+                states_actions_returns.append((state, action, returns))
+            returns = DISCOUNT * returns + reward
+        states_actions_returns.reverse()
 
-        statesAndActions = []
-        for state, action, G in statesActionsReturns:
-            if (state, action) not in statesAndActions:
-                pairsVisited[(state,action)] += 1
-                returns[(state,action)] += (1 / pairsVisited[(state,action)])*(G-returns[(state,action)])
-                Q[(state,action)] = returns[(state,action)]
-                statesAndActions.append((state,action))
-                values = np.array([Q[(state,a)] for a in grid.possibleActions])
-                best = np.random.choice(np.where(values==values.max())[0])
+        states_and_actions = []
+        for state, action, returns in states_actions_returns:
+            if (state, action) not in states_and_actions:
+                PAIRS_VISITED[(state, action)] += 1
+                RETURNS[(state, action)] += (
+                    (1 / PAIRS_VISITED[(state, action)])
+                    * (returns - RETURNS[(state, action)])
+                )
+                ESTIMATES[(state, action)] = RETURNS[(state, action)]
+                states_and_actions.append((state, action))
+                values = np.array([
+                    ESTIMATES[(state, a)] for a in GRID.possible_actions
+                ])
+                best = np.random.choice(np.where(values == values.max())[0])
                 rand = np.random.random()
-                if rand < 1 - EPS:
-                    policy[state] = grid.possibleActions[best]
+                if rand < 1 - EPSILON:
+                    POLICY[state] = GRID.possible_actions[best]
                 else:
-                    policy[state] = np.random.choice(grid.possibleActions)
+                    POLICY[state] = np.random.choice(GRID.possible_actions)
 
-    printQ(Q, grid)
-    printPolicy(policy,grid)
+    print_estimates(ESTIMATES, GRID)
+    print_policy(POLICY, GRID)
