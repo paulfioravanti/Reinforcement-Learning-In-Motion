f1a9d0f04adae842801afb10ff5bcdb0b67f3516
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-11-29 21:50:21 +1100

    Refactor frozen lake

--- Unit-6-The-Windy-Gridworld/frozen_lake.py
+++ Unit-6-The-Windy-Gridworld/frozen_lake.py
@@ -3,74 +3,83 @@ import gym
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    env = gym.make("FrozenLake-v0")
-    GAMMA = 0.9
-    EPS = 0.1
-    possibleActions = [0, 1, 2, 3]
-    stateSpace = [i for i in range(16)]
-    Q = {}
-    returns = {}
-    pairsVisited = {}
-    for state in stateSpace:
-        for action in possibleActions:
-            Q[(state, action)] = 0
-            returns[(state,action)] = 0
-            pairsVisited[(state,action)] = 0
+    ENV = gym.make("FrozenLake-v0")
+    DISCOUNT = 0.9
+    EPSILON = 0.1
+    POSSIBLE_ACTIONS = [0, 1, 2, 3]
+    STATE_SPACE = list(range(16))
+    ESTIMATES = {}
+    RETURNS = {}
+    PAIRS_VISITED = {}
 
-    policy = {}
-    for state in stateSpace:
-        policy[state] = np.random.choice(possibleActions)
+    for state in STATE_SPACE:
+        for action in POSSIBLE_ACTIONS:
+            ESTIMATES[(state, action)] = 0
+            RETURNS[(state, action)] = 0
+            PAIRS_VISITED[(state, action)] = 0
 
-    for i in range(100000):
-        statesActionsReturns = []
-        if i % 5000 == 0:
-            print('starting episode', i)
-        observation = env.reset()
+    POLICY = {}
+    for state in STATE_SPACE:
+        POLICY[state] = np.random.choice(POSSIBLE_ACTIONS)
+
+    NUM_EPISODES = 100000
+    STATUS_INTERVAL = 5000
+
+    for i in range(NUM_EPISODES):
+        states_actions_returns = []
+        if i % STATUS_INTERVAL == 0:
+            print("starting episode", i)
+        observation = ENV.reset()
         memory = []
         done = False
         while not done:
-            action = policy[observation]
-            observation_, reward, done, info = env.step(action)
+            action = POLICY[observation]
+            observation_, reward, done, info = ENV.step(action)
             memory.append((observation, action, reward))
             observation = observation_
 
         memory.append((observation, action, reward))
-        G = 0
+        returns = 0
         last = True # start at t = T - 1
         for state, action, reward in reversed(memory):
             if last:
                 last = False
             else:
-                statesActionsReturns.append((state,action,G))
-            G = GAMMA*G + reward
+                states_actions_returns.append((state, action, returns))
+            returns = DISCOUNT * returns + reward
 
-        statesActionsReturns.reverse()
-        statesAndActions = []
-        for state, action, G in statesActionsReturns:
-            if (state, action) not in statesAndActions:
-                pairsVisited[(state,action)] += 1
-                returns[(state,action)] += (1 / pairsVisited[(state,action)])*(G-returns[(state,action)])
-                Q[(state,action)] = returns[(state,action)]
-                statesAndActions.append((state,action))
-                values = np.array([Q[(state,a)] for a in possibleActions])
-                best = np.random.choice(np.where(values==values.max())[0])
+        states_actions_returns.reverse()
+        states_and_actions = []
+        for state, action, returns in states_actions_returns:
+            if (state, action) not in states_and_actions:
+                PAIRS_VISITED[(state, action)] += 1
+                RETURNS[(state, action)] += (
+                    (1 / PAIRS_VISITED[(state, action)])
+                    * (returns - RETURNS[(state, action)])
+                )
+                ESTIMATES[(state, action)] = RETURNS[(state, action)]
+                states_and_actions.append((state, action))
+                values = np.array(
+                    [ESTIMATES[(state, a)] for a in POSSIBLE_ACTIONS]
+                )
+                best = np.random.choice(np.where(values == values.max())[0])
                 rand = np.random.random()
-                if rand < 1 - EPS:
-                    policy[state] = possibleActions[best]
+                if rand < 1 - EPSILON:
+                    POLICY[state] = POSSIBLE_ACTIONS[best]
                 else:
-                    policy[state] = np.random.choice(possibleActions)
-    numGames = 1000
-    rewards = np.zeros(numGames)
-    epRewards = 0
-    for i in range(numGames):
-        observation = env.reset()
+                    POLICY[state] = np.random.choice(POSSIBLE_ACTIONS)
+    NUM_GAMES = 1000
+    REWARDS = np.zeros(NUM_GAMES)
+    EPISODE_REWARDS = 0
+    for i in range(NUM_GAMES):
+        observation = ENV.reset()
         done = False
         while not done:
-            action = policy[observation]
-            observation_, reward, done, info = env.step(action)
+            action = POLICY[observation]
+            observation_, reward, done, info = ENV.step(action)
             observation = observation_
-            epRewards += reward
-        rewards[i] = epRewards
-    print (epRewards/numGames)
-    plt.plot(rewards)
+            EPISODE_REWARDS += reward
+        REWARDS[i] = EPISODE_REWARDS
+    print(EPISODE_REWARDS / NUM_GAMES)
+    plt.plot(REWARDS)
     plt.show()
